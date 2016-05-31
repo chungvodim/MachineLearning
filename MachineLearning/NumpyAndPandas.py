d2b7b57d0db31c1f5ef9da0c47a0df0a6b77c2c4
Long Nguyen <ba198647@gmail.com>
2016-06-01 00:18:07 +0800

    up

--- MachineLearning/NumpyAndPandas.py
+++ MachineLearning/NumpyAndPandas.py
@@ -1,112 +1,188 @@
 import unicodecsv
+import pandas as pd
+import numpy as np
 
-def read_csv(filename):
-    with open(filename, 'rb') as f:
-        reader = unicodecsv.DictReader(f)
-        return list(reader)
+#def read_csv(filename):
+#    with open(filename, 'rb') as f:
+#        reader = unicodecsv.DictReader(f)
+#        return list(reader)
 
-enrollments = read_csv('data\IntroToDataAnalysis\enrollments.csv')
-daily_engagement = read_csv('data\IntroToDataAnalysis\daily_engagement.csv')
-project_submissions = read_csv('data\IntroToDataAnalysis\project_submissions.csv')
+#enrollments = read_csv('data\IntroToDataAnalysis\enrollments.csv')
+#daily_engagement = read_csv('data\IntroToDataAnalysis\daily_engagement.csv')
+#project_submissions = read_csv('data\IntroToDataAnalysis\project_submissions.csv')
+#full_daily_engagement = pd.read_csv('data\IntroToDataAnalysis\daily_engagement_full.csv')
 
-udacity_test_accounts = set()
-for enrollment in enrollments:
-    if enrollment['is_udacity'] == 'True':
-        udacity_test_accounts.add(enrollment['account_key'])
-print 'number of test accounts: {}'.format(len(udacity_test_accounts))
+#print 'daily_engagement_full: {}'.format(len(full_daily_engagement['acct'].unique()))
 
-def remove_udacity_accounts(data):
-    non_udacity_data = []
-    for data_point in data:
-        if data_point['account_key'] not in udacity_test_accounts:
-            non_udacity_data.append(data_point)
-    return non_udacity_data
+# First 20 countries with employment data
+countries = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
+])
 
-def get_unique_students(data):
-    unique_students = set()
-    for data_point in data:
-        unique_students.add(data_point['account_key'])
-    return unique_students
+# Employment data in 2007 for those 20 countries
+employment = np.array([
+    55.70000076,  51.40000153,  50.5       ,  75.69999695,
+    58.40000153,  40.09999847,  61.5       ,  57.09999847,
+    60.90000153,  66.59999847,  60.40000153,  68.09999847,
+    66.90000153,  53.40000153,  48.59999847,  56.79999924,
+    71.59999847,  58.40000153,  70.40000153,  41.20000076
+])
 
-unique_enrolled_students = get_unique_students(enrollments)
-print 'number of enrollment: {}'.format(len(enrollments))
-print 'number of unique enrollment: {}'.format(len(unique_enrolled_students))
-# Change the key name
-for engagement_record in daily_engagement:
-    engagement_record['account_key'] = engagement_record['acct']
-    del[engagement_record['acct']]
+# Change False to True for each block of code to see what it does
 
-unique_engagement_students = get_unique_students(daily_engagement)
-print 'number of engagement: {}'.format(len(daily_engagement))
-print 'number of unique engagement: {}'.format(len(unique_engagement_students))
+# Accessing elements
+print 'Accessing elements'
+if True:
+    print countries[0]
+    print countries[3]
 
-unique_project_submitters = get_unique_students(project_submissions)
-print 'number of submissions: {}'.format(len(project_submissions))
-print 'number of unique submissions: {}'.format(len(unique_project_submitters))
+# Slicing
+print 'Slicing'
+if True:
+    print countries[0:3]
+    print countries[:3]
+    print countries[17:]
+    print countries[:]
 
-#for enrollment in enrollments:
-#    student = enrollment['account_key']
-#    if student not in unique_engagement_students:
-#        print enrollment
-#        break
-### Checking For More Problem Records
+# Element types
+print 'Element types'
+if True:
+    print countries.dtype
+    print employment.dtype
+    print np.array([0, 1, 2, 3]).dtype
+    print np.array([1.0, 1.5, 2.0, 2.5]).dtype
+    print np.array([True, False, True]).dtype
+    print np.array(['AL', 'AK', 'AZ', 'AR', 'CA']).dtype
 
-num_problem_students = 0
-for enrollment in enrollments:
-    student = enrollment['account_key']
-    if (student not in unique_engagement_students and enrollment['join_date'] != enrollment['cancel_date']):
-        num_problem_students += 1
-print 'Problem Records: {}'.format(num_problem_students)
+# Looping
+if True:
+    for country in countries:
+        print 'Examining country {}'.format(country)
 
+    for i in range(len(countries)):
+        country = countries[i]
+        country_employment = employment[i]
+        print 'Country {} has employment {}'.format(country,
+                country_employment)
 
+# Numpy functions
+if True:
+    print 'mean of employment: {}'.format(employment.mean())
+    print 'standard deviation of employment: {}'.format(employment.std())
+    print 'max of employment: {}'.format(employment.max())
+    print 'sum of employment: {}'.format(employment.sum())
 
-non_udacity_enrollments = remove_udacity_accounts(enrollments)
-non_udacity_engagement = remove_udacity_accounts(daily_engagement)
-non_udacity_submissions = remove_udacity_accounts(project_submissions)
+def max_employment(countries, employment):
+    '''
+    Fill in this function to return the name of the country
+    with the highest employment in the given employment
+    data, and the employment in that country.
+    '''
+    #max_country = countries[employment.argmax()]
+    max_country = countries[np.argmax(employment)]      # Replace this with your code
+    max_value = employment.max()   # Replace this with your code
+    print 'max_country: {}'.format(max_country)
+    print 'max_value: {}'.format(max_value)
+    return (max_country, max_value)
 
-print 'number of non_udacity_enrollment: {}'.format(len(non_udacity_enrollments))
-print 'number of non_udacity_engagement: {}'.format(len(non_udacity_engagement))
-print 'number of non_udacity_submissions: {}'.format(len(non_udacity_submissions))
+max_employment(countries,employment)
 
-# Refining the question
-paid_students = {}
-for enrollment in non_udacity_enrollments:
-    if not enrollment['is_canceled'] or enrollment['days_to_cancel'] > 7:
-        account_key = enrollment['account_key']
-        enrollment_date = enrollment['join_date']
-        if account_key not in paid_students or enrollment_date > paid_students[account_key]:
-            paid_students[account_key] = enrollment_date
-print 'number of paid student: {}'.format(len(paid_students))
+# Change False to True for each block of code to see what it does
 
-# Getting Data from First Week
-from datetime import datetime
+# Arithmetic operations between 2 NumPy arrays
+if True:
+    a = np.array([1, 2, 3, 4])
+    b = np.array([1, 2, 1, 2])
+    
+    print a + b
+    print a - b
+    print a * b
+    print a / b
+    print a ** b
+    
+# Arithmetic operations between a NumPy array and a single number
+if True:
+    a = np.array([1, 2, 3, 4])
+    b = 2
+    
+    print a + b
+    print a - b
+    print a * b
+    print a / b
+    print a ** b
+    
+# Logical operations with NumPy arrays
+if True:
+    a = np.array([True, True, False, False])
+    b = np.array([True, False, True, False])
+    
+    print a & b
+    print a | b
+    print ~a
+    
+    print a & True
+    print a & False
+    
+    print a | True
+    print a | False
+    
+# Comparison operations between 2 NumPy Arrays
+if True:
+    a = np.array([1, 2, 3, 4, 5])
+    b = np.array([5, 4, 3, 2, 1])
+    
+    print a > b
+    print a >= b
+    print a < b
+    print a <= b
+    print a == b
+    print a != b
+    
+# Comparison operations between a NumPy array and a single number
+if True:
+    a = np.array([1, 2, 3, 4])
+    b = 2
+    
+    print a > b
+    print a >= b
+    print a < b
+    print a <= b
+    print a == b
+    print a != b
+    
+# First 20 countries with school completion data
+countries = np.array([
+       'Algeria', 'Argentina', 'Armenia', 'Aruba', 'Austria','Azerbaijan',
+       'Bahamas', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Bolivia',
+       'Botswana', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi',
+       'Cambodia', 'Cameroon', 'Cape Verde'
+])
 
-def within_one_week(join_date, engagement_date):
-    time_delta = datetime.strptime(engagement_date, '%Y-%m-%d') - datetime.strptime(join_date, '%Y-%m-%d')
-    return time_delta.days < 7
+# Female school completion rate in 2007 for those 20 countries
+female_completion = np.array([
+    97.35583,  104.62379,  103.02998,   95.14321,  103.69019,
+    98.49185,  100.88828,   95.43974,   92.11484,   91.54804,
+    95.98029,   98.22902,   96.12179,  119.28105,   97.84627,
+    29.07386,   38.41644,   90.70509,   51.7478 ,   95.45072
+])
 
-def remove_free_trial_cancels(data):
-    new_data = []
-    for data_point in data:
-        if data_point['account_key'] in paid_students:
-            new_data.append(data_point)
-    return new_data
+# Male school completion rate in 2007 for those 20 countries
+male_completion = np.array([
+     95.47622,  100.66476,   99.7926 ,   91.48936,  103.22096,
+     97.80458,  103.81398,   88.11736,   93.55611,   87.76347,
+    102.45714,   98.73953,   92.22388,  115.3892 ,   98.70502,
+     37.00692,   45.39401,   91.22084,   62.42028,   90.66958
+])
 
-paid_enrollments = remove_free_trial_cancels(non_udacity_enrollments)
-paid_engagement = remove_free_trial_cancels(non_udacity_engagement)
-paid_submissions = remove_free_trial_cancels(non_udacity_submissions)
-
-print 'number of paid enrollments: {0}'.format(len(paid_enrollments))
-print 'number of paid engagement: {0}'.format(len(paid_engagement))
-print 'number of paid submissions: {0}'.format(len(paid_submissions))
-
-paid_engagement_in_first_week = []
-for engagement_record in paid_engagement:
-    account_key = engagement_record['account_key']
-    join_date = paid_students[account_key]
-    engagement_record_date = engagement_record['utc_date']
-
-    if within_one_week(join_date, engagement_record_date):
-        paid_engagement_in_first_week.append(engagement_record)
-
-print 'number of paid engagement in first week: {0}'.format(len(paid_engagement_in_first_week))
+def overall_completion_rate(female_completion, male_completion):
+    '''
+    Fill in this function to return a NumPy array containing the overall
+    school completion rate for each country. The arguments are NumPy
+    arrays giving the female and male completion of each country in
+    the same order.
+    '''
+    return None
