64d01e89e7843aa95654872c8db0c507a46bbefc
chungvodim <ba198647@gmail.com>
2016-06-12 16:45:51 +0700

    up

--- MachineLearning/Naive_Bayes/nb_author_id.py
+++ MachineLearning/Naive_Bayes/nb_author_id.py
@@ -27,7 +27,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-
+def classify(features_train, labels_train):
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf.fit(features_train,labels_train)
+    return clf
 #########################################################
 
 
