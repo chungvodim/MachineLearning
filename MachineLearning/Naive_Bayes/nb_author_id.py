cf1816b6072750273b2990493a37664c3bccd4fd
chungvodim <ba198647@gmail.com>
2016-06-12 19:59:15 +0700

    finish 1st project

--- MachineLearning/Naive_Bayes/nb_author_id.py
+++ MachineLearning/Naive_Bayes/nb_author_id.py
@@ -12,7 +12,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("C:/WorkSpace/MachineLearning/MachineLearning/tools")
 from email_preprocess import preprocess
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
@@ -21,7 +21,8 @@ from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+#print sys.path
+features_train, features_test, labels_train, labels_test = preprocess(words_file = "C:/WorkSpace/MachineLearning/MachineLearning/tools/word_data.pkl", authors_file="C:/WorkSpace/MachineLearning/MachineLearning/tools/email_authors.pkl")
 
 
 
@@ -31,8 +32,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 def classify(features_train, labels_train,features_test,labels_test):
     clf = GaussianNB()
+    t0 = time()
     clf.fit(features_train,labels_train)
+    print "training time:", round(time()-t0, 3), "s"
+    t0 = time()
     pred = clf.predict(features_test)
+    print "prediction time:", round(time()-t0, 3), "s"
     accuracy = accuracy_score(pred, labels_test)
     return accuracy
 
