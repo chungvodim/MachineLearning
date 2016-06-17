90edcb40af3d477e04228c5d7536e4d3cb5a09da
chungvodim <ba198647@gmail.com>
2016-06-17 17:42:51 +0700

    add OutLier

--- MachineLearning/Regression/regressionQuiz.py
+++ MachineLearning/Regression/regressionQuiz.py
@@ -19,27 +19,31 @@ reg.fit(ages_train, net_worths_train)
 ### argument to your prediction function is in the expected format - if you get
 ### a warning about needing a 2d array for your data, a list of lists will be
 ### interpreted by sklearn as such (e.g. [[27]]).
-km_net_worth = 1.0 ### fill in the line of code to get the right value
-
+km_net_worth = reg.predict([[27]]) ### fill in the line of code to get the right value
+print km_net_worth
 ### get the slope
 ### again, you'll get a 2-D array, so stick the [0][0] at the end
-slope = 0. ### fill in the line of code to get the right value
-
+slope = reg.coef_ ### fill in the line of code to get the right value
+print slope
 ### get the intercept
 ### here you get a 1-D array, so stick [0] on the end to access
 ### the info we want
-intercept = 0. ### fill in the line of code to get the right value
-
+intercept = reg.intercept_ ### fill in the line of code to get the right value
+print intercept
 
 ### get the score on test data
-test_score = 0. ### fill in the line of code to get the right value
-
+test_score = reg.score(ages_test,net_worths_test) ### fill in the line of code to get the right value
+print test_score
 
 ### get the score on the training data
-training_score = 0. ### fill in the line of code to get the right value
+training_score = reg.score(ages_train,net_worths_train) ### fill in the line of code to get the right value
+print training_score
 
-
-
+plt.scatter(ages_train,net_worths_train)
+plt.plot(ages_train,reg.predict(ages_train),color='blue',linewidth=3)
+plt.xlabel("ages")
+plt.ylabel("net worth")
+plt.show()
 def submitFit():
     # all of the values in the returned dictionary are expected to be
     # numbers for the purpose of the grader.
